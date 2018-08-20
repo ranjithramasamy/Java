4d9066ea3a4d8eef40687ab1cd08fae26179180b
Ramasamy <XBBKFJK@pac.bnymellon.net>
2018-08-19 20:32:21 -0400

    Java Algorithms Practice

---
+++ src/com/self/string/Palindrome.java
@@ -0,0 +1,47 @@
+package com.self.string;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String input = "A man, a plan, a canal. Panama";
+		boolean palindrome = isPalindrome(input.toCharArray());
+		
+		System.out.println("isPalindrome: " + palindrome);
+	}
+	
+	private static boolean isPalindrome(char[] input) {
+		boolean isPalindrome = true;
+		int start = 0;
+		int end = input.length - 1;
+		Pattern regex = Pattern.compile("[^A-Za-z0-9]");
+				
+		while(start < end){
+			boolean posChanged = false;
+			Matcher smatch = regex.matcher(String.valueOf(input[start]));
+			Matcher ematch = regex.matcher(String.valueOf(input[end]));
+
+			if(smatch.find()) {
+				start++;
+				posChanged = true;
+			}
+			
+			if(ematch.find()) {
+				end--;
+				posChanged = true;
+			}
+			
+			if(String.valueOf(input[start]).equalsIgnoreCase(String.valueOf(input[end]))) {
+				start++;
+				end--;
+			} else if(!posChanged) {
+				isPalindrome = false;
+				break;
+			}
+		}
+		
+		return isPalindrome;
+	}
+}
---
+++ src/com/self/string/StringReverseWithSpecialChars.java
@@ -0,0 +1,44 @@
+package com.self.string;
+
+public class StringReverseWithSpecialChars {
+
+	public static void main(String[] args) {
+		String input = "a,b$fgd*";
+		String output = reverse(input);
+		String outputWithoutSpecialChars = reverseWithoutSpecialChars(input);
+		
+		System.out.println("Reversed String: " + output);
+		System.out.println("Reversed String wihtout Special Characters: " + outputWithoutSpecialChars);
+	}
+	
+	private static String reverse(String input) {
+		if(input.length() == 1){
+			return input;
+		}
+		
+		return input.substring(input.length()-1) + reverse(input.substring(0, input.length()-1));
+	}
+	
+	private static String reverseWithoutSpecialChars(String actual) {
+		char[] strArr = actual.toCharArray();
+		int start = 0;
+		int end = strArr.length - 1;
+		
+		while(start < end){
+			if(!Character.isAlphabetic(strArr[start])){
+				start++;
+			}else if(!Character.isAlphabetic(strArr[end])){
+				end--;
+			}else{
+				char currChar = strArr[end];
+				strArr[end] = strArr[start];
+				strArr[start] = currChar;
+				
+				start++;
+				end--;
+			}
+		}
+
+		return String.valueOf(strArr);
+	}
+}
