8cf530bb1fc3ba3d1aa3397c0ca2b44598446f45
Ranjith Ramasamy <ranjith.ramasamy@hotmail.com>
2019-01-30 23:33:57 +0530

    Added substring palindrome count

---
+++ Algorithms/src/com/self/string/SubStringPalindromeCount.java
@@ -0,0 +1,44 @@
+
+public class SubStringPalindromeCount {
+	
+	public static void main(String[] args) {
+		System.out.println("Sub String Palindrome Count is: " + countPalindromes("sasasas"));
+	}
+	
+	public static int countPalindromes(String s) {
+		int count = 0;
+		
+		for(int oIdx = 0; oIdx < s.length(); oIdx++) {
+			for(int iIdx = oIdx + 1; iIdx <= s.length(); iIdx++) {
+				String subStr = s.substring(oIdx, iIdx);
+				
+				if(subStr.length() == 1) {
+					count++;
+				}else if(isPalindrome(subStr)) {
+					count++;
+				}
+			}
+		}
+
+		return count;
+    }
+	
+	private static boolean isPalindrome(String str){
+		char[] tmpStr = str.toCharArray();
+        int start = 0;
+        int end = tmpStr.length - 1;
+        boolean result = true;
+                
+        while(start < end) {
+        	if(tmpStr[start] != tmpStr[end]) {
+        		result = false;
+        		break;
+        	}
+        	
+        	start++;
+        	end--;
+        }
+        
+        return result;
+    }
+}
