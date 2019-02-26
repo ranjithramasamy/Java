81f631e5e7880ba36dedb788858bf1410014e828
Ranjith Ramasamy <ranjith.ramasamy@risksense.com>
2019-02-26 18:59:05 +0530

    Added String manipulation algorithm

---
+++ Algorithms/src/com/self/DeletionDistanceBetweenStrings.java
@@ -0,0 +1,51 @@
+package com.self;
+
+public class DeletionDistanceBetweenStrings {
+	public static void main(String[] args) {
+		System.out.println(deletionDistance("heat", "hit"));
+	}
+	
+	private static int deletionDistance(String str1, String str2) {
+		if(str1.equals(str2)) {
+			return 0;
+		}
+		
+		if("".equals(str1)) {
+			return str2.length();
+		}
+		
+		if("".equals(str2)) {
+			return str1.length();
+		}
+		
+		int oIdx = 0;
+		int iIdx = 0;
+		int distance = 0;
+		String tmpStr1 = "";
+		String tmpStr2 = "";
+		String deletedStr = "";
+		int str2Pos = 0;
+		
+		for(oIdx = 0; oIdx < str1.length(); oIdx++) {
+			String oSubStr = str1.substring(oIdx, oIdx + 1);
+			
+			for(iIdx = str2Pos; iIdx < str2.length(); iIdx++) {
+				String iSubStr = str2.substring(iIdx, iIdx + 1);
+				
+				if(oSubStr.equals(iSubStr)) {
+					str2Pos++;
+					tmpStr1 = str1.substring(oIdx + 1);
+					break;
+				}
+			}
+			
+			if(iIdx == str2.length()) {
+				deletedStr += oSubStr;
+				tmpStr1 = str1.substring(oIdx + 1);
+				str2Pos = 0;
+			}
+		}
+		
+		return distance;
+	}
+}
---
+++ Algorithms/src/com/self/RemoveDuplicateOfString.java
@@ -0,0 +1,32 @@
+package com.self;
+
+public class RemoveDuplicateOfString {
+
+	public static void main(String[] args) {		
+		String input = "geeksforgeeks";
+		boolean lower = true;
+		
+		System.out.println(approach1(input, lower));
+		System.out.println((char)('a' - 2));
+	}
+	
+	private static String approach1(String input, boolean lower) {
+		int[] visited = new int[25];
+		StringBuilder output = new StringBuilder();
+		int idx = 0;
+		char lowerChar = lower ? 'a' : 'A';
+		
+		while(idx < input.length()) {
+			char current = input.charAt(idx++);
+			int pos = current - lowerChar;
+
+			if(visited[pos] == 0) {
+				visited[pos] = 1;
+				output.append(current);
+			}
+		}
+		
+		return output.toString();
+	}
+
+}
